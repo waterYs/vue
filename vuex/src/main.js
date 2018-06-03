4549217712b34c0dc3d08a9c6e58b56b09241f34
zhangweijian <2694597925@qq.com>
2018-06-03 21:00:33 +0800

    入口文件main.js

--- vuex/src/main.js
+++ vuex/src/main.js
@@ -2,17 +2,20 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-// import router from './router'  //引入router文件中的index.js
+import router from './router'  //引入router文件中的index.js
 import store from './store/store'  //引入store.js
+// import echarts from 'echarts' //引入echarts 图表js
 
 
 Vue.config.productionTip = false
+// Vue.prototype.$echarts=echarts
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  // router,//配置路由
+  router,//配置路由
   store,//配置store
+  // echarts,
   components: { App },
   template: '<App/>'
 })
