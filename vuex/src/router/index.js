54ad5b942923707cf7f09ba17b737d00c938f299
zhangweijian <2694597925@qq.com>
2018-06-03 20:59:17 +0800

    router 中index.js 路由

--- vuex/src/router/index.js
+++ vuex/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Counter from '../components/Counter'  //引入Counter组件
+import Echarts from '@/components/Echarts'
 
 Vue.use(Router);
 
@@ -9,8 +10,13 @@ var router=new Router({
 	routes:[
 		{
 			path:'/',
-			name:'Counter',
+			name:'counter',
 			component:Counter
+		},
+		{
+			path:'/echarts',
+			name:'echarts',
+			component:Echarts
 		}
 	]
 })
